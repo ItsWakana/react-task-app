b3f109f86d147ca5885651c319debf5eab794903
wakana <wakanawardrobe@gmail.com>
2023-03-24 16:48:11 +0000

    add events for handling input and setting it to state

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
+import Overview from './Overview';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      tasks: []
+      id: 0,
+      tasks: [{id: 10, name: 'First Task'}],
+      inputValue: ''
     }
+
+    this.addTask = this.addTask.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value,
+    })
+  }
+
+  addTask(e) {
+    console.log(e);
+    const task = {id: this.state.id, name: this.state.inputValue};
+
+    this.setState({
+      id: this.state.id + 1,
+      tasks: [...this.state.tasks, task]
+    })
   }
 
   render() {
     return (
       <div>
-        <input type="search" placeholder='Enter Task'></input>
-        <button type='submit'>Submit Task</button>
+        <input type="text" onChange={this.handleInputChange}></input>
+        <button type='submit' onClick={this.addTask}>Submit Task</button>
+        <Overview tasks={this.state.tasks}/>
       </div>
     )
   }
