8c23804f188b7b312ef968478e447d328550ca3b
wakana <wakanawardrobe@gmail.com>
2023-03-27 14:22:38 +0100

    add edit section to edit task

---
+++ src/components/EditText.js
@@ -0,0 +1,11 @@
+const EditText = (props) => {
+
+    return (
+        <div>
+            <input placeholder="Enter edit here"></input>
+            <button type="submit">Save edit</button>
+        </div>
+    )
+}
+
+export default EditText;
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
+import EditText from './EditText';
 
 const Overview = (props) => {
     const { tasks } = props;
 
+    const [isEditing, setIsEditing] = useState(false);
+
     const handleEdit = (e) => {
         e.preventDefault();
+        setIsEditing(true);
+
     }
     return (
-        <ul class="task-container">
+        <ul className="task-container">
             {tasks.map(task => (
-                <li class="task" key={task.id}>
-                    <div class="task__name">
+                <li className="task" key={task.id}>
+                    <div className="task__name">
                         <p><b>Task Name:</b></p> 
                         <p>{task.name}</p>
                     </div>
-                    <div class="task__id">
+                    <div className="task__id">
                         <b>ID:</b> {task.id}
                     </div>
 
                     <button onClick={handleEdit}>Edit Task</button>
+                    {isEditing && <EditText />}
                 </li>
             ))}
         </ul>
