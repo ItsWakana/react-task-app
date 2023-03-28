bf2e58b3026dd304fe8c075af93f78170069d354
wakana <wakanawardrobe@gmail.com>
2023-03-28 14:20:58 +0100

    remove unused function components and old code

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,36 +1,6 @@
 import React from 'react';
 import EditText from './EditText';
 
-// const Overview = (props) => {
-//     const { tasks } = props;
-
-//     const [isEditing, setIsEditing] = useState(false);
-
-//     const handleEdit = (e) => {
-//         e.preventDefault();
-//         setIsEditing(true);
-
-//     }
-//     return (
-//         <ul className="task-container">
-//             {tasks.map(task => (
-//                 <li className="task" key={task.id}>
-//                     <div className="task__name">
-//                         <p><b>Task Name:</b></p> 
-//                         <p>{task.name}</p>
-//                     </div>
-//                     <div className="task__id">
-//                         <b>ID:</b> {task.id}
-//                     </div>
-
-//                     <button onClick={handleEdit}>Edit Task</button>
-//                     {isEditing && <EditText />}
-//                 </li>
-//             ))}
-//         </ul>
-//     )
-// }
-
 class Overview extends React.Component {
 
     constructor(props) {
@@ -43,16 +13,6 @@ class Overview extends React.Component {
         this.handleEditCompletion = this.handleEditCompletion.bind(this);
 
     }
-    // const { tasks } = props;
-
-    // const [isEditing, setIsEditing] = useState(false);
-
-    // handleEdit(e) {
-    //     e.preventDefault();
-    //     this.setState({
-    //         isEditing: !this.state.isEditing,
-    //     });
-    // }
 
     handleEditCompletion() {
         this.setState({
@@ -63,7 +23,6 @@ class Overview extends React.Component {
     render() {
 
         const { tasks, onTaskEdit, onEditPhase } = this.props;
-        const { isEditing } = this.state;
         return (
             <ul className="task-container">
                 {tasks.map((task, index) => (
