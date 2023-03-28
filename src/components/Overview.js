d615435ad8d448bdd18f1d8c0c4ae4db63d65128
wakana <wakanawardrobe@gmail.com>
2023-03-28 11:47:17 +0100

    turn into class component and hide edit button on click

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,35 +1,80 @@
 import React from 'react';
-import { useState } from 'react';
 import EditText from './EditText';
 
-const Overview = (props) => {
-    const { tasks } = props;
+// const Overview = (props) => {
+//     const { tasks } = props;
 
-    const [isEditing, setIsEditing] = useState(false);
+//     const [isEditing, setIsEditing] = useState(false);
 
-    const handleEdit = (e) => {
-        e.preventDefault();
-        setIsEditing(true);
+//     const handleEdit = (e) => {
+//         e.preventDefault();
+//         setIsEditing(true);
+
+//     }
+//     return (
+//         <ul className="task-container">
+//             {tasks.map(task => (
+//                 <li className="task" key={task.id}>
+//                     <div className="task__name">
+//                         <p><b>Task Name:</b></p> 
+//                         <p>{task.name}</p>
+//                     </div>
+//                     <div className="task__id">
+//                         <b>ID:</b> {task.id}
+//                     </div>
+
+//                     <button onClick={handleEdit}>Edit Task</button>
+//                     {isEditing && <EditText />}
+//                 </li>
+//             ))}
+//         </ul>
+//     )
+// }
+
+class Overview extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isEditing: false,
+        }
+
+        this.handleEdit = this.handleEdit.bind(this)
 
     }
-    return (
-        <ul className="task-container">
-            {tasks.map(task => (
-                <li className="task" key={task.id}>
-                    <div className="task__name">
-                        <p><b>Task Name:</b></p> 
-                        <p>{task.name}</p>
-                    </div>
-                    <div className="task__id">
-                        <b>ID:</b> {task.id}
-                    </div>
+    // const { tasks } = props;
 
-                    <button onClick={handleEdit}>Edit Task</button>
-                    {isEditing && <EditText />}
-                </li>
-            ))}
-        </ul>
-    )
+    // const [isEditing, setIsEditing] = useState(false);
+
+    handleEdit(e) {
+        e.preventDefault();
+        this.setState({
+            isEditing: !this.state.isEditing,
+        });
+
+    }
+    render() {
+
+        const { tasks } = this.props;
+        const { isEditing } = this.state;
+        return (
+            <ul className="task-container">
+                {tasks.map(task => (
+                    <li className="task" key={task.id}>
+                        <div className="task__name">
+                            <p><b>Task Name:</b></p> 
+                            <p>{task.name}</p>
+                        </div>
+                        <div className="task__id">
+                            <b>ID:</b> {task.id}
+                        </div>
+                        {!isEditing &&                         <button onClick={this.handleEdit}>Edit Task</button>}
+                        {isEditing && <EditText />}
+                    </li>
+                ))}
+            </ul>
+        )
+    }
 }
-
 export default Overview;
